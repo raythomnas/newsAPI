8ecb900acd65c209ca34426c1141fb32857a6590
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-02-19 05:32:57 +0000

    search top working, search all wip

--- js/script.js
+++ js/script.js
@@ -3,8 +3,25 @@ key = k[0].key;
 
 $(document).ready(function(){
 
+//swaps between search all and search top
+document.getElementById('allButton').addEventListener('click', function(){
+  document.getElementById('topButton').style.backgroundColor = "white";
+  document.getElementById('allButton').style.backgroundColor = "grey";
+  document.getElementById('topSection').style.display = "none";
+  document.getElementById('allSection').style.display = "block";
+  console.log('clicked');
+});
+
+document.getElementById('topButton').addEventListener('click', function(){
+  document.getElementById('allButton').style.backgroundColor = "white";
+  document.getElementById('topButton').style.backgroundColor = "grey";
+  document.getElementById('allSection').style.display = "none";
+  document.getElementById('topSection').style.display = "block";
+});
+
+// generic search for on load & testing, as well as everything version for testing
 var test = `http://newsapi.org/v2/top-headlines?country=nz&apiKey=${key}`;
-var test2 = `http://newsapi.org/v2/everything?q=${keyword}&apiKey=${key}`;
+// var test = `http://newsapi.org/v2/everything?language=ud&apiKey=${key}`;
 
 function general(){
   $.ajax({
@@ -45,55 +62,95 @@ function generalTest(x){
 //search vars
 var keyword = "";
 var country = "";
-var category = "";
+var category = ""; 
 
 //checks for search url
 var keyCheck = null;
 var countryCheck = null;
 var categoryCheck = null;
 
-general();
+general(); // fill page with preset options
 
 document.getElementById('searchButton').addEventListener('click', function(e){
+
   keyword = document.getElementById('searchWord').value; 
   console.log(keyword);
-  test2 = `http://newsapi.org/v2/everything?q=${keyword}&apiKey=${key}`;
-  // orient = document.getElementById('orient').value;
-  // console.log(orient)
-  // number = document.getElementById('numbers').value;
-  // console.log(number)
+  country = document.getElementById('country').value;
+  console.log(country);
+  category = document.getElementById('category').value;
+  console.log(category);
   document.getElementById('dump').innerHTML = ""; // clear page
-  var x = test2; // set which search htttp to use
+
+  paramCheck(); // checks which options are selected and which vars to set
+
+  var x = url; // set which search http to use
 
   generalTest(x); //fire off funcitno
   });
-});
-
-// var input = document.getElementById("searchWord");
-// // Execute a function when the user releases a key on the keyboard
-// input.addEventListener("keyup", function(event) {
-//   // Number 13 is the "Enter" key on the keyboard
-//   if (event.keyCode === 13) {
-//     // Cancel the default action, if needed
-//     event.preventDefault();
-//     // Trigger the button element with a click
-//     document.getElementById("searchButton").click();
-//   }
-// });
-
-// console.log('changes');
 
 $(document).on('change','#category',function(){
-    alert('Change Happened');
     categoryCheck = "checked";
+    console.log(categoryCheck);
 });
 
 $(document).on('change','#searchWord',function(){
-    alert('Change Happened');
     keyCheck = "checked";
+    console.log(keyCheck);
 });
 
 $(document).on('change','#country',function(){
-    alert('Change Happened');
     countryCheck = "checked";
+    console.log(countryCheck);
+});
+
+
+//checks for every set of parameters to be selected and places relavent url into url var
+function paramCheck(){
+if ((keyCheck === null) && (categoryCheck === null) && (countryCheck === null)) {
+  console.log('all null');
+} else if ((keyCheck === "checked") && (categoryCheck === null) && (countryCheck === null)) {
+  console.log('key checked');
+  url = `http://newsapi.org/v2/top-headlines?q=${keyword}&apiKey=${key}`;
+} else if ((keyCheck === null) && (categoryCheck === "checked") && (countryCheck === null)) {
+  console.log('categoryCheck checked');
+  url = `http://newsapi.org/v2/top-headlines?category=${category}&apiKey=${key}`;
+} else if ((keyCheck === null) && (categoryCheck === null) && (countryCheck === "checked")) {
+  console.log('country checked');
+  url = `http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${key}`;
+} else if ((keyCheck === "checked") && (categoryCheck === "checked") && (countryCheck === null)) {
+  console.log('key and category checked');
+  url = `http://newsapi.org/v2/top-headlines?q=${keyword}&category=${category}&apiKey=${key}`;
+} else if ((keyCheck === "checked") && (categoryCheck === null) && (countryCheck === "checked")) {
+  console.log('key and country checked');
+  url = `http://newsapi.org/v2/top-headlines?q=${keyword}&country=${country}&apiKey=${key}`;
+} else if ((keyCheck === null) && (categoryCheck === "checked") && (countryCheck === "checked")) {
+  console.log('category and country checked');
+  url = `http://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${key}`;
+} else if ((keyCheck === "checked") && (categoryCheck === "checked") && (countryCheck === "checked")) {
+  console.log('all checked');
+  url = `http://newsapi.org/v2/top-headlines?q=${keyword}&country=${country}&category=${category}&apiKey=${key}`;
+}
+}
+
+url = `http://newsapi.org/v2/top-headlines?country=nz&apiKey=${key}`;
+
+// end of param checks for top section
+
+// functino for checking all paramaters
+
+$(document).on('change','#searchWord2',function(){
+    document.getElementById('country2').innerHTML += '<option value="relevancy">Relevance</option>';
+    console.log("key2Check");
+});
+
+
+//executes search function onclick while focused on keyword entry area
+var input = document.getElementById("searchWord");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("searchButton").click();
+  }
+});
+
 });
